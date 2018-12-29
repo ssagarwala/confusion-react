bfa2a8ba5677b661da5671d833ac00c4474c95a9
Sehar <sehar.sagarwala@gmail.com>
2018-12-29 11:43:35 -0600

    Fetch Handling Errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -15,11 +15,27 @@ export const addComment = (dishId, rating, author, comment) =>({
         }
 });
 
+
 export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
     return fetch(baseUrl +'dishes')
+            .then(response => {
+                if(response.ok){
+                    return response;
+                }
+                else{
+                    var error= new Error('Error ' + response.status + ':' + response.statusText );
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => {
+                var errmess = new Error(error.message)
+                throw errmess;
+            })
             .then (response=> response.json() )
-            .then(dishes => dispatch(addDishes(dishes)));
+            .then(dishes => dispatch(addDishes(dishes)))
+            .catch(error => dispatch(dishesFailed(error.message)));
 }
 
 export const dishesLoading = () => ({
@@ -37,9 +53,25 @@ export const addDishes = (dishes) => ({
 });
 
 export const fetchComments = () => (dispatch) => {    
-    return fetch(baseUrl +'comments')
-            .then (response=> response.json() )
-            .then(comments => dispatch(addComments(comments)));
+        return fetch(baseUrl +'comments')
+        .then(response => {
+            if(response.ok){
+                return response;
+            }
+            else{
+                var error= new Error('Error ' + response.status + ':' + response.statusText );
+                error.response = response;
+                throw error;
+            }
+        },
+        error => {
+            var errmess = new Error(error.message)
+            throw errmess;
+        })
+    
+        .then (response=> response.json() )
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)));
 }
 export const commentsFailed = (errmess) => ({
     type:ActionTypes.COMMENTS_FAILED,
@@ -53,8 +85,23 @@ export const addComments = (comments) => ({
 export const fetchPromos = () => (dispatch) => {
     dispatch(promosLoading(true));
     return fetch(baseUrl +'promotions')
-            .then (response=> response.json() )
-            .then(promos => dispatch(addPromos(promos)));
+        .then(response => {
+            if(response.ok){
+                return response;
+            }
+            else{
+                var error= new Error('Error ' + response.status + ':' + response.statusText );
+                error.response = response;
+                throw error;
+            }
+        },
+        error => {
+            var errmess = new Error(error.message)
+            throw errmess;
+        })
+        .then (response=> response.json() )
+        .then(promos => dispatch(addPromos(promos)))
+        .catch(error => dispatch(promosFailed(error.message)));
 }
 
 export const promosLoading = () => ({
