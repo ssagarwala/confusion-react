3411a518d7ac60633492895c7de0ab61d3be942a
Sehar <sehar.sagarwala@gmail.com>
2018-12-30 17:30:05 -0600

    React animation components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import {FadeTransform, Fade, Stagger} from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) =>!(val) || (val.length <=len);
@@ -107,13 +108,18 @@ function RenderDish({dish}){
         if(dish != null){
             return(
                 <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg width="100%" src={baseUrl +dish.image} alt={dish.name}/>
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText> {dish.description}</CardText>
-                        </CardBody>
-                    </Card>
+                    <FadeTransform in 
+                    transformProps ={{
+                        exitTransform:'scale(0.5) translateY(-50%)'
+                    }}>
+                            <Card>
+                                <CardImg width="100%" src={baseUrl +dish.image} alt={dish.name}/>
+                                <CardBody>
+                                    <CardTitle>{dish.name}</CardTitle>
+                                    <CardText> {dish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                    </FadeTransform>
                 </div>               
             );
         }
@@ -130,15 +136,19 @@ function RenderDish({dish}){
                 <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
                     <ul className="list-unstyled">
-                        {commentsArr.map((comment)=>{
-                            return(
-                                <li key={comment.id}>
-                                <p>{comment.comment}</p>
-                                <p>-- {comment.author},
-                                {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                                </li>
-                             );
-                        })}
+                        <Stagger in>
+                            {commentsArr.map((comment)=>{
+                                return(
+                                    <Fade in>
+                                        <li key={comment.id}>
+                                        <p>{comment.comment}</p>
+                                        <p>-- {comment.author},
+                                        {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                        </li>
+                                    </Fade>
+                                );
+                            })}
+                        </Stagger>
                     </ul>
                     <CommentForm dishId={dishId} postComment={postComment} />     
                 </div>
